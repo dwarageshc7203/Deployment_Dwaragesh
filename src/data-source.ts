d4fa20a1782738497f3b436ae8396cad0c95c7f9
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-06-30 17:45:13 +0530

    test

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,8 +15,8 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [Doctor, Patient, User, Appointment, Timeslot, DoctorAvailability],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
+  migrations: [],
+  synchronize: true,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
