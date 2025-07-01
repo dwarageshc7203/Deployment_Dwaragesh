5fa965cf5cb26f2e44b4e22623765a64482461a6
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-07-01 19:05:01 +0530

    testing

--- src/migrations/1751373400930-AddSlotDurationAndReportingTime.ts
+++ src/migrations/1751373400930-AddSlotDurationAndReportingTime.ts
@@ -1,11 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class AddSlotDurationAndReportingTime1751373400930 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn("doctor", new TableColumn({
+      name: "slot_duration",
+      type: "integer",
+      isNullable: false,
+      default: 15
+    }));
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+    await queryRunner.addColumn("doctor", new TableColumn({
+      name: "patients_per_slot",
+      type: "integer",
+      isNullable: false,
+      default: 3
+    }));
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("doctor", "patients_per_slot");
+    await queryRunner.dropColumn("doctor", "slot_duration");
+  }
 }
