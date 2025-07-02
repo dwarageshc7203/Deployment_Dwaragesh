6a5666c05e7ba272d603e1da1e72550afd8ade73
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-07-02 09:13:51 +0530

    Add reporting_time to appointment

---
+++ src/migrations/1751427782177-AddReportingTimeToAppointment.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddReportingTimeToAppointment1751427782177 implements MigrationInterface {
+    name = 'AddReportingTimeToAppointment1751427782177'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "appointment" ADD COLUMN "reporting_time" VARCHAR(10)
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE "appointment" DROP COLUMN "reporting_time"
+    `);
+  }
+
+}
