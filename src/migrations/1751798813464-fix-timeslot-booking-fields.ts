047a550d5e9612f9bb909170736a6d3c4a025bd6
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-07-06 16:37:16 +0530

    testing 17

--- src/migrations/1751798813464-fix-timeslot-booking-fields.ts
+++ src/migrations/1751798813464-fix-timeslot-booking-fields.ts
@@ -1,19 +1,35 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class FixTimeslotBookingFields1751798813464 implements MigrationInterface {
+  name = 'FixTimeslotBookingFields1751798813464';
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "timeslot"
-      ALTER COLUMN "booking_start_time" TYPE timestamp USING booking_start_time::timestamp,
-      ALTER COLUMN "booking_end_time" TYPE timestamp USING booking_end_time::timestamp;
+      ALTER TABLE "timeslot" DROP COLUMN "booking_start_time";
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" DROP COLUMN "booking_end_time";
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" ADD "booking_start_time" TIMESTAMP;
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" ADD "booking_end_time" TIMESTAMP;
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      ALTER TABLE "timeslot"
-      ALTER COLUMN "booking_start_time" TYPE time USING booking_start_time::time,
-      ALTER COLUMN "booking_end_time" TYPE time USING booking_end_time::time;
+      ALTER TABLE "timeslot" DROP COLUMN "booking_start_time";
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" DROP COLUMN "booking_end_time";
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" ADD "booking_start_time" TIME;
+    `);
+    await queryRunner.query(`
+      ALTER TABLE "timeslot" ADD "booking_end_time" TIME;
     `);
   }
 }
