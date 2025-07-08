a492c673346d107fe37006d7593c29074bfb52d4
dwarageshc7203 <dwarageshc7203@gmail.com>
2025-07-08 21:14:50 +0530

    appointment

--- src/entities/appointment.entity.ts
+++ src/entities/appointment.entity.ts
@@ -37,11 +37,11 @@ time_slot: Timeslot;
   @Column({ type: 'enum', enum: ['morning', 'evening'], nullable: true })
   session: 'morning' | 'evening';
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', length: 20 })
   appointment_status: string;
 
-  @Column({ type: 'varchar' })
-  reason: string;
+@Column({ type: 'varchar', length: 255 })  // adjust as needed
+reason: string;
 
   @Column('text')
   notes: string;
